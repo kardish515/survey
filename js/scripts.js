047a1edb945158217bc9eada1c96839c2657768d
Matt Miller and Nick Kardish <nick@>
2018-03-21 14:46:03 -0700

    update options in index.html and add javascript functionality to favorite color.

---
+++ js/scripts.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+  // $("#color").click(function(event) {
+    var favoriteColorSelected = $("#color").val();
+  //   event.preventDefault();
+  //   console.log(favoriteColorSelected);
+  //
+  //   $("#favoriteColor").css( "color", "favoriteColorSelected" );
+  // });
+
+
+});
